6eeefbfcafcccf730dc553e61a729a059d49f462
king <117745900@qq.com>
2021-01-12 14:37:54 +0800

    update

--- packages/orbit-res/nginx/start-nginx.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# if [[ ! -d /opt/app/dist ]]; then
-#   echo "invalid ENV";
-#   exit 1;
-# fi
-
-# exec nginx -g "pid /tmp/nginx.pid; daemon off;"
-nginx -g "pid /tmp/nginx.pid; daemon off;"
